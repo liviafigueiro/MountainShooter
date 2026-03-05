1b87d17a5296a6c999dabfd1b0f0ce3d68f6c3a5
Lívia <liviafigueiroramos@gmail.com>
2026-03-05 17:29:12 -0300

    Level 2 implementation

--- code/Const.py
+++ code/Const.py
@@ -13,6 +13,7 @@ C_CYAN = (0, 128, 128)
 
 #E
 EVENT_ENEMY = pygame.USEREVENT +1
+EVENT_TIMEOUT = pygame.USEREVENT +2
 
 ENTITY_DAMAGE = {
     'Level1Bg0': 0,
@@ -22,14 +23,19 @@ ENTITY_DAMAGE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 1,
     'Player1Shot': 35,
     'Player2': 1,
     'Player2Shot': 20,
     'Enemy1': 1,
-    'Enemy1Shot': 15,
+    'Enemy1Shot': 25,
     'Enemy2': 1,
-    'Enemy2Shot': 20
+    'Enemy2Shot': 40
 
 }
 
@@ -41,6 +47,11 @@ ENTITY_SCORE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -60,6 +71,11 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
     'Player1': 3,
     'Player1Shot': 1,
     'Player2': 3,
@@ -87,6 +103,11 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -111,7 +132,13 @@ PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT, 'Player2': pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT, 'Player2': pygame.K_d}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL, 'Player2': pygame.K_LCTRL}
 
-SPAWN_TIME = 4000
+
+#S
+SPAWN_TIME = 3000
+
+#T
+TIMEOUT_STEP = 100
+TIMEOUT_LEVEL = 20000 #20 segundos
 
 #W
 WIN_WIDTH = 576
--- code/entityFactory.py
+++ code/entityFactory.py
@@ -15,10 +15,18 @@ class EntityFactory:
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
-                for i in range(7):
+                for i in range(7): #numero de img do level1
                     list_bg.append(Background(f'Level1Bg{i}', (0,0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+
+            case 'Level2Bg':
+                list_bg = []
+                for i in range(5):
+                    list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+
             case 'Player1':
                 return Player('Player1', (10,WIN_HEIGHT/3))
             case 'Player2':
